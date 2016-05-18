34637259d84584dcff214493fd0019b770af3f5d
tvand7093 <tyler.j.vanderhoef@gmail.com>
2016-05-18 15:23:44 -0700

    More tweaks for azure.

--- install.sh
+++ install.sh
@@ -27,19 +27,19 @@ FULL_NPM="npm"
 if [ $AZURE -eq 1 ]
 then
   # not azuer, so use local npm
-  FULL_NPM=node "$NPM"
+  FULL_NPM="node \"$NPM\""
 fi
 
 echo Using npm version
-$FULL_NPM -v
+eval $FULL_NPM -v
 
 # Ensure strict ssl is turned off. This is an Azure issue.
-$FULL_NPM config set strict-ssl false
+eval $FULL_NPM config set strict-ssl false
 
 echo Installing kajero
 
 # setup kajero
-$FULL_NPM install -g kajero
+eval $FULL_NPM install -g kajero
 
 echo Done installing kajero
 
